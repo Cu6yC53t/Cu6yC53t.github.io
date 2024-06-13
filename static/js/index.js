b459b8cd21e4985a3309131cca15af8f71d1eae9
Cu6yC53t <171793086+Cu6yC53t@users.noreply.github.com>
2024-06-13 14:24:50 +0100

    Add js code for dropdown

--- static/js/index.js
+++ static/js/index.js
@@ -1,42 +1,98 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+async function loadVideoData() {
+  try {
+    const response = await fetch('static/videos.json');
+    const videoData = await response.json();
+    return videoData;
+  } catch (error) {
+    console.error('Error fetching JSON:', error);
+    return {};
+  }
+}
 
+function appendQueryParams(url) {
+  // Extract the video ID from the URL
+  const urlParts = url.split('?');
+  const baseUrl = urlParts[0];
+  const queryString = urlParts[1];
+  const videoId = baseUrl.split('/').pop();
+
+  // Create new query parameters
+  const newParams = `autoplay=1&loop=1&playlist=${videoId}&mute=1`;
+
+  // Return the new URL with the appended query parameters
+  return `${baseUrl}?${queryString}&${newParams}`;
+}
+
+
+function setResultVideo(task, method, videoData) {
+  const videoUrls = videoData[`${task}/${method}`];
+  if (!videoUrls) {
+    console.error('Video URLs not found for:', `${task}/${method}`);
+    return;
+  }
+
+  ids = ["#train-scene", "#test-scene-1", "#test-scene-2", "#test-scene-3"];
+  for (i = 0; i < ids.length; i++) {
+    $(ids[i]).attr("src", appendQueryParams(videoUrls[i]));
+  }
+}
+
+async function handleMenuChange() {
+  const videoData = await loadVideoData();
+  const taskValue = $('#single-menu-task').val();
+  const methodValue = $('#single-menu-method').val();
+  setResultVideo(taskValue, methodValue, videoData);
+}
+
+
+$(document).ready(async function () {
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+  });
+
+  var options = {
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    loop: true,
+    infinite: true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+  }
+
+  // Initialize all div with carousel class
+  var carousels = bulmaCarousel.attach('.carousel', options);
+
+  // Loop on each carousel initialized
+  for (var i = 0; i < carousels.length; i++) {
+    // Add listener to  event
+    carousels[i].on('before:show', state => {
+      console.log(state);
     });
+  }
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
-    }
+  // Access to bulmaCarousel instance of an element
+  var element = document.querySelector('#my-element');
+  if (element && element.bulmaCarousel) {
+    // bulmaCarousel instance is available as element.bulmaCarousel
+    element.bulmaCarousel.on('before-show', function (state) {
+      console.log(state);
+    });
+  }
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
+  $('#single-menu-task, #single-menu-method').on("change", handleMenuChange);
 
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
+  bulmaSlider.attach();
 
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
-    bulmaSlider.attach();
+  // Load initial data and set initial iframe src values
+  const videoData = await loadVideoData();
+  const initialTaskValue = $('#single-menu-task').val();
+  const initialMethodValue = $('#single-menu-method').val();
+  setResultVideo(initialTaskValue, initialMethodValue, videoData);
 })
